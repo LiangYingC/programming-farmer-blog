8cb3a0fd4fd42d75018840d5b1b7dcf79214c52a
Liang Chen <liangchen@91app.com>
2021-01-30 23:12:22 +0800

    feat : test eslint and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 const Homepage: FC = () => {
+  const test = ['q3'];
+
+  const testFilter = test.filter(value => value === 'q3');
+
+  useEffect(() => {
+    console.log(test);
+  }, []);
+
   return <h1>Welcome to the Homepage!</h1>;
-}
+};
 
 export default Homepage;
