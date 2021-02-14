f0e887fb758608965f3f4cda7475a6a3d2e1bea8
Liang Chen <liangchen@91app.com>
2021-02-14 23:54:12 +0800

    feat : init AllPostsPage

---
+++ pages/posts/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { Text } from '@chakra-ui/react';
+
+const PostsPage: FC = () => {
+  return <Text>Welcome to the AllPostsPage!</Text>;
+};
+
+export default PostsPage;
