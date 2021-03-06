a128ba733cf5bef8832b9801dc8e6743de1443fb
Liang Chen <liangchen@91app.com>
2021-03-06 17:15:39 +0800

    feat : implement desktop PostContent componsnt

---
+++ components/PostContent/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import ReactMarkdown from 'react-markdown/with-html';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import Layout from '@components/Layout';
+import { Article } from '@components/PostContent/indexStyle';
+
+const CodeBlock: FC<{ language: string; value: string }> = ({ language, value }) => {
+  return (
+    <SyntaxHighlighter
+      language={language}
+      style={atomOneDark}
+      customStyle={{ background: '#090c14' }}
+    >
+      {value}
+    </SyntaxHighlighter>
+  );
+};
+
+const PostContent: FC<{ content: string }> = ({ content }) => {
+  return (
+    <Layout>
+      <Article>
+        <ReactMarkdown escapeHtml={false} source={content} renderers={{ code: CodeBlock }} />
+      </Article>
+    </Layout>
+  );
+};
+
+export default PostContent;
---
+++ components/PostContent/indexStyle.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Article = styled.article`
+  a {
+    display: inline;
+    box-shadow: inset 0 -5px 0 0 rgba(255, 255, 255, 0.3);
+    transition: 0.25s;
+
+    :hover {
+      box-shadow: inset 0 0 0 0;
+    }
+  }
+`;
