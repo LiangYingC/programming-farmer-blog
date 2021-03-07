e1c84e15bf6144794f71bf3fae812271a3f64668
Liang Chen <liangchen@91app.com>
2021-03-07 22:43:10 +0800

    refactor : refactor fs lib

--- lib/fs.ts
+++ lib/fs.ts
@@ -1,48 +1,14 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import { categoryPaths } from '@configs/paths';
-
-/**
- * Get file name and category of all posts
- * @return [{category, postFile} , ...]
- */
-export const getAllPostFiles = () => {
-  const allPostfiles = categoryPaths.reduce((prev, category) => {
-    const categoryName = category.name;
-
-    const postsDirectory = `${process.cwd()}/contents/posts/${categoryName}`;
-    const postFiles = fs.readdirSync(postsDirectory);
-
-    const postFilesData = postFiles.map(postFile => {
-      return {
-        category: categoryName,
-        postFile: postFile,
-      };
-    });
-
-    return [...prev, ...postFilesData];
-  }, [] as { category: string; postFile: string }[]);
-
-  return allPostfiles;
-};
-
-/**
- * Get post file names of a specific category
- * @param categoryName - post category name，example：javaScript、react
- */
-export const getPostFilesByCategory = (categoryName: string) => {
-  const postsDirectory = `${process.cwd()}/contents/posts/${categoryName}`;
-  const postFiles = fs.readdirSync(postsDirectory);
-
-  return postFiles;
-};
+import { Frontmatter, Posts } from '@myTypes/post';
 
 /**
  * Get slug of a post
- * @param postFiles - a post file name
+ * @param postFileName - a post file name
  */
-export const getPostSlug = (postFile: string) => {
-  return postFile.replace('.md', '');
+export const getPostSlug = (postFileName: string) => {
+  return postFileName.replace('.md', '');
 };
 
 /**
@@ -52,6 +18,63 @@ export const getPostSlug = (postFile: string) => {
 export const getPostMatter = (postFilePath: string) => {
   const markdownFileContent = fs.readFileSync(postFilePath, 'utf8').toString();
   const { data, content } = matter(markdownFileContent);
+  const frontmatter = {
+    ...data,
+    date: JSON.stringify(data.date),
+  } as Frontmatter;
 
-  return { frontmatter: data, content };
+  return { frontmatter, content };
+};
+
+/**
+ * Make and return posts data by postFileNames and categoryName
+ * @param postFileNames - post file names of posts, ex: [javascript-parseInt-parseFloat-Number, javascript-var-let-const-for-loop]
+ * @param categoryName - a category name of posts, ex: JavaScript
+ * @return [{ category, slug, frontmatter } , ...]
+ */
+const makePosts = (postFileNames: string[], categoryName: string) => {
+  const posts = postFileNames.map(postFileName => {
+    // post frontmatter data
+    const postFilePath = `${process.cwd()}/contents/posts/${categoryName}/${postFileName}`;
+    const { frontmatter } = getPostMatter(postFilePath);
+
+    // post slug data
+    const postSlug = getPostSlug(postFileName);
+
+    return {
+      category: categoryName,
+      slug: postSlug,
+      frontmatter,
+    };
+  });
+
+  return posts;
+};
+
+/**
+ * Get posts data of a specific category
+ * @param categoryName - post category name，example：JavaScript、React
+ * @return [{ category, slug, frontmatter } , ...]
+ */
+export const getPostsByCategory = (categoryName: string) => {
+  const postsDirectory = `${process.cwd()}/contents/posts/${categoryName}`;
+  const postFileNames = fs.readdirSync(postsDirectory);
+  const posts = makePosts(postFileNames, categoryName);
+
+  return posts;
+};
+
+/**
+ * Get all posts data by categoryPaths config
+ * @return [{ category, slug, frontmatter } , ...]
+ */
+export const getAllPosts = () => {
+  const allPosts = categoryPaths.reduce((prev, category) => {
+    const categoryName = category.name;
+    const posts = getPostsByCategory(categoryName);
+
+    return [...prev, ...posts];
+  }, [] as Posts);
+
+  return allPosts;
 };
