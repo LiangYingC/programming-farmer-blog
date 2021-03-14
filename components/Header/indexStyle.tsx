d1a2aeedc528e5c50bb46b78d1947ff88a64af16
Liang Chen <liangchen@91app.com>
2021-03-14 17:58:18 +0800

    feat : implement Header tabletS RWD

--- components/Header/indexStyle.tsx
+++ components/Header/indexStyle.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 
-export const HeaderWrapper = styled.header<{ isHide: boolean; isShowHeaderBgColor: boolean }>`
+export const HeaderWrapper = styled.header<{
+  isHide: boolean;
+  isShowHeaderBgColor: boolean;
+}>`
   position: fixed;
   top: ${({ isHide }) => (isHide ? '-70px' : '0px')};
   left: 50%;
@@ -11,6 +14,12 @@ export const HeaderWrapper = styled.header<{ isHide: boolean; isShowHeaderBgColo
   background-color: ${({ isShowHeaderBgColor, theme }) =>
     isShowHeaderBgColor ? theme.colors.bgColor2 : 'transparent'};
   transition: 0.2s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    top: ${({ isHide }) => (isHide ? '-55px' : '0px')};
+    height: 50px;
+    background-color: ${({ theme }) => theme.colors.bgColor2};
+  }
 `;
 
 export const InnerWrapper = styled.div`
@@ -20,21 +29,33 @@ export const InnerWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    justify-content: center;
+  }
 `;
 
 export const Title = styled.h1`
   font-size: ${({ theme }) => theme.fontSizes['4xl']};
   filter: brightness(0.9);
-  transition: 0.25s;
+  transition: 0.2s;
 
   :hover {
     filter: brightness(1);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    font-size: ${({ theme }) => theme.fontSizes.xl};
   }
 `;
 
 export const Nav = styled.nav`
   display: flex;
   align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    display: none;
+  }
 `;
 
 export const NavItem = styled.div`
@@ -62,4 +83,18 @@ export const ColorModeIcon = styled.div`
   font-size: ${({ theme }) => theme.fontSizes['4xl']};
   color: ${({ theme }) => theme.colors.heightLightColor};
   transition: 0.25s;
+`;
+
+export const MobileHeaderWrapper = styled.header<{
+  isHide: boolean;
+}>`
+  position: fixed;
+  top: ${({ isHide }) => (isHide ? '-55px' : '0px')};
+  left: 50%;
+  z-index: ${({ theme }) => theme.zIndexs.navigater};
+  transform: translateX(-50%);
+  height: 55px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.bgColor2};
+  transition: 0.2s;
 `;
