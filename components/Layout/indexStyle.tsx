9a95a319caa0e7a956111559e8e5cd038d483312
Liang Chen <liangchen@91app.com>
2021-03-14 22:57:19 +0800

    feat : implment RWD about Layout content

--- components/Layout/indexStyle.tsx
+++ components/Layout/indexStyle.tsx
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 
 export const Content = styled.main`
   max-width: 800px;
+  padding: 15px;
   margin: 0 auto;
   padding-top: 65px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    padding-top: 45px;
+  }
 `;
