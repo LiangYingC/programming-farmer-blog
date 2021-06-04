8e18432d6e4b8ae7d857095e1153a95fbe8390e6
Liang Chen <liangchen@91app.com>
2021-06-04 23:42:53 +0800

    feat : remove forwardRef IconBtn on Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, forwardRef } from 'react';
+import React, { FC } from 'react';
 import Link from 'next/link';
 import { FaReact } from 'react-icons/fa';
 import { IoLogoJavascript, IoLogoCss3 } from 'react-icons/io';
@@ -25,20 +25,6 @@ const mobileFooterConfig = [
   },
 ];
 
-/**
- * If the Link child is a function component, need to use React.forwardRef to solve “Warning: Function components cannot be given refs.”
- * ref : https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-function-component
- */
-const IconBtn = forwardRef<HTMLAnchorElement, { iconElemenet: ReactNode; href: string }>(
-  function IconBtn({ iconElemenet, href }, ref) {
-    return (
-      <FooterIcon href={href} ref={ref}>
-        {iconElemenet}
-      </FooterIcon>
-    );
-  }
-);
-
 const Footer: FC = () => {
   return (
     <>
@@ -51,8 +37,8 @@ const Footer: FC = () => {
       <MobileFooterWrapper>
         {mobileFooterConfig.map(({ name, component }) => {
           return (
-            <Link key={name} href={`/articles/${name}`} passHref>
-              <IconBtn iconElemenet={component} href={`/articles/${name}`} />
+            <Link key={name} href={`/articles/${name}`}>
+              <FooterIcon href={`/articles/${name}`}>{component}</FooterIcon>
             </Link>
           );
         })}
