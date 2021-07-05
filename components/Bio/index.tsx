aa9a2492176430b4e204ed7f964ef06a551cca85
Liang C <sl20130313@gmail.com>
2021-07-05 21:55:57 +0800

    refactor : extract Bio Styles

--- components/Bio/index.tsx
+++ components/Bio/index.tsx
@@ -1,8 +1,17 @@
 import { FC, useState, useEffect } from 'react';
 import Image from 'next/image';
-import styled from '@emotion/styled';
 import { useColorMode } from '@contexts/ColorModeContext';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import {
+  BioWrapper,
+  RoundImageWrapper,
+  Intro,
+  Title,
+  Divider,
+  Description,
+  SocialLinks,
+  SocialLink,
+} from '@components/Bio/indexStyle';
 
 const socialLinks = [
   {
@@ -31,82 +40,6 @@ const myImages = {
     alt: 'Picture of the author at day.',
   },
 };
-
-const BioWrapper = styled.div`
-  margin-top: 60px;
-  display: flex;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
-    margin-top: 30px;
-    flex-direction: column;
-  }
-`;
-
-const RoundImageWrapper = styled.div`
-  flex: none;
-  width: 250px;
-  height: 250px;
-  border-radius: 50%;
-  overflow: hidden;
-  margin-right: 40px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
-    margin: 0 auto 20px auto;
-  }
-`;
-
-const Intro = styled.div`
-  flex: auto;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Title = styled.h2`
-  flex: none;
-  margin: 15px 0;
-  font-size: ${({ theme }) => theme.fontSizes['3xl']};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileL}) {
-    font-size: ${({ theme }) => theme.fontSizes.xl};
-    margin: 0 auto;
-  }
-`;
-
-const Divider = styled.span`
-  padding: 0 25px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileL}) {
-    padding: 0 10px;
-  }
-`;
-
-const Description = styled.p`
-  flex: auto;
-  font-size: ${({ theme }) => theme.fontSizes.md};
-`;
-
-const SocialLinks = styled.div`
-  flex: none;
-  display: flex;
-  margin-bottom: 20px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileL}) {
-    margin-bottom: 10px;
-  }
-`;
-
-const SocialLink = styled.div`
-  font-size: ${({ theme }) => theme.fontSizes['4xl']};
-  padding-top: 5px;
-  padding-right: 10px;
-  filter: brightness(0.95);
-  transition: 0.25s;
-
-  :hover {
-    filter: brightness(1);
-    cursor: pointer;
-  }
-`;
 
 const Bio: FC = () => {
   const { isDarkMode } = useColorMode();
