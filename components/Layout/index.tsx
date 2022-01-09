af992e3aac4d737c9b953bda864adf0ed2d2cdb1
Liang C <sl20130313@gmail.com>
2022-01-09 21:59:20 +0800

    fix: add domain to meta image link

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -20,19 +20,30 @@ const Layout: FC<LayoutProps> = ({ children, pageTitle, pageDesc, pageURL, pageT
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="shortcut icon" href="/assets/icons/favicon-32x32.png" />
+        <link
+          rel="shortcut icon"
+          href="https://www.programfarmer.com/assets/icons/favicon-32x32.png"
+        />
         <title>{pageTitle}</title>
         <meta name="description" content={pageDesc} key="desc" />
         <meta property="og:type" content={pageType} key="ogType" />
         <meta property="og:title" content={pageTitle} key="ogTitle" />
         <meta property="og:description" content={pageDesc} key="ogDesc" />
         <meta property="og:url" content={pageURL} key="ogUrl" />
-        <meta property="og:image" content="/assets/icons/icon-384x384.png" key="ogImage" />
+        <meta
+          property="og:image"
+          content="https://www.programfarmer.com/assets/icons/icon-384x384.png"
+          key="ogImage"
+        />
         <meta property="og:site_name" content="城市碼農" key="ogSiteName" />
         <meta name="twitter:title" content={pageTitle} key="twitterTitle" />
         <meta name="twitter:description" content={pageDesc} key="twitterDesc" />
         <meta name="twitter:url" content={pageURL} key="twitterPageUrl" />
-        <meta name="twitter:image" content="/assets/icons/icon-384x384.png" key="twitterImage" />
+        <meta
+          name="twitter:image"
+          content="https://www.programfarmer.com/assets/icons/icon-384x384.png"
+          key="twitterImage"
+        />
         <meta name="twitter:creator" content="@LiangCh95173853" key="twitterCreator" />
         {isHomePage && (
           <meta
