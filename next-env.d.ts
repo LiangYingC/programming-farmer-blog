dfec08ea5921d3081bfd99d1c006066a82fca234
Liang C <sl20130313@gmail.com>
2022-01-11 21:40:23 +0800

    chore: update next version to v12.0.7

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
-// A file named next-env.d.ts will be created in the root of your project.
-// This file ensures Next.js types are picked up by the TypeScript compiler.
-// You cannot remove it, however, you can edit it (but you don't need to).
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
