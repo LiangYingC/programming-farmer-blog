bcd53a1dda4cda511b06ff01bc30e7204e20ec97
Liang C <sl20130313@gmail.com>
2022-01-20 19:16:23 +0800

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider, Global } from '@emotion/react';
 import { ColorModeProvider, useColorMode } from '@contexts/ColorModeContext';
 import { defaultTheme, darkThemeColors, lightThemeColors } from '@styles/theme';
 import getGlobalStyle from '@styles/getGlobalStyle';
+
+const GTAGE_ID = process.env.GTAGE_ID;
 
 const ThemeContainer = ({ children }: { children: React.ReactNode }) => {
   const { isDarkMode } = useColorMode();
@@ -22,6 +25,19 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ColorModeProvider>
       <ThemeContainer>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTAGE_ID}`}
+        />
+        <Script id="ga-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${GTAGE_ID});
+          `}
+        </Script>
         <Component {...pageProps} />
       </ThemeContainer>
     </ColorModeProvider>
