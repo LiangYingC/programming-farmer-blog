4a0845baca8a798aa04a3f1a1c2b3b7ef4176f67
Liang C <sl20130313@gmail.com>
2022-01-22 16:15:17 +0800

    refacto: Articles to articles

--- lib/sort.ts
+++ lib/sort.ts
@@ -3,11 +3,12 @@ import { parseJsonStrToDate } from '@lib/format';
 
 /**
  * Sort Articles from newest to oldest
+ * @param articles - article data list
  */
-export const sortArticlesByDateDesc = (Articles: Articles) => {
-  Articles.sort((a, b) => {
+export const sortArticlesByDateDesc = (articles: Articles) => {
+  articles.sort((a, b) => {
     return parseJsonStrToDate(a.frontmatter.date) > parseJsonStrToDate(b.frontmatter.date) ? -1 : 1;
   });
 
-  return Articles;
+  return articles;
 };
