3decbe3bb50c0756458fa69b252e807772738463
Liang C <sl20130313@gmail.com>
2023-04-25 22:40:15 +0800

    feat: add article constants file

---
+++ constants/articles.ts
@@ -0,0 +1,28 @@
+export const ARTICLE_YAERS = [2019, 2020, 2021, 2022, 2023];
+
+export const ARTICLE_TAG_PATHS = [
+  {
+    name: 'react',
+    path: '/react',
+  },
+  {
+    name: 'sourceCode',
+    path: '/sourceCode',
+  },
+  {
+    name: 'javaScript',
+    path: '/javaScript',
+  },
+  {
+    name: 'style',
+    path: '/style',
+  },
+];
+
+export const ARTICLE_ALL_PATHS = [
+  {
+    name: 'all',
+    path: '/',
+  },
+  ...ARTICLE_TAG_PATHS,
+];
