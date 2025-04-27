fa09bc5052c2ab26ce819826f77ed8227e5b9fe8
liang.chen <liang.chen@ichef.com.tw>
2025-04-27 18:02:58 +0800

    chore: add i18n setting on next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   compiler: {
     emotion: true,
   },
+  i18n: {
+    locales: ['zh-TW', 'en-US'],
+    defaultLocale: 'zh-TW',
+  },
   async redirects() {
     return [
       {
