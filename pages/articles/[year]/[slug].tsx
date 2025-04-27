52a8f992f1f9b4de9cc14a117bd37a27631dfd00
liang.chen <liang.chen@ichef.com.tw>
2025-04-27 20:52:27 +0800

    feat: make slug pages (article pages) support locale

--- pages/articles/[year]/[slug].tsx
+++ pages/articles/[year]/[slug].tsx
@@ -1,7 +1,10 @@
+import fs from 'fs';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { Frontmatter } from '@myTypes/articles';
 import { getAllArticles, getArticleMatter } from '@lib/fs';
 import Article from '@components/Article';
+import { Locale } from '@myTypes/locale';
+import { DEFAULT_LOCALE } from '@constants/locales';
 
 interface ArticlePageProps {
   frontmatter: Frontmatter;
@@ -24,30 +27,49 @@ const ArticlePage = ({
   );
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
   const slug = (params?.slug || '') as string;
   const year = (params?.year || '') as string;
-  const articleFilePath = `${process.cwd()}/contents/articles/${year}/${slug}.md`;
+  const currentLocale = (locale || DEFAULT_LOCALE) as Locale;
+
+  const localeArticlePath = `${process.cwd()}/contents/articles/${year}/${currentLocale}/${slug}.md`;
+  const defaultArticlePath = `${process.cwd()}/contents/articles/${year}/${slug}.md`;
+
+  let articleFilePath = defaultArticlePath;
+  try {
+    if (fs.existsSync(localeArticlePath)) {
+      articleFilePath = localeArticlePath;
+    }
+  } catch (err) {
+    articleFilePath = defaultArticlePath;
+  }
+
+  const domain = process.env.SITE_URL || 'https://www.programfarmer.com';
   const { frontmatter, content } = getArticleMatter(articleFilePath);
 
   return {
     props: {
       content: `# ${frontmatter.title}\n${content}`,
       frontmatter,
-      articleUrl: `https://www.programfarmer.com/articles/${year}/${slug}`,
+      articleUrl: `${domain}/${currentLocale}/articles/${year}/${slug}`,
     },
   };
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const allArticles = getAllArticles();
-  const paths = allArticles.map(({ slug, year }) => {
-    return {
-      params: {
-        slug,
-        year,
-      },
-    };
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  const supportedLocales = locales || [DEFAULT_LOCALE];
+
+  const paths = supportedLocales.flatMap((locale) => {
+    const allArticles = getAllArticles(locale as Locale);
+    return allArticles.map(({ slug, year }) => {
+      return {
+        params: {
+          slug,
+          year,
+        },
+        locale,
+      };
+    });
   });
 
   return {
